e8ce63159edd1fee7eb6ba164436a63bbdf14fdc
TelmoPersch <119141423+telmopersch123@users.noreply.github.com>
2025-10-01 14:01:54 -0400

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button className="my-1 bg-red-500 px-5">Click me</Button>
+      <Button className="my-1 px-5">Click me</Button>
     </div>
   );
 }
